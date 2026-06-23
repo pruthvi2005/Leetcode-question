acf12a5aaee997a1debdfe8de49542e46bbc49ec
pruthvi reddy <110444967+pruthvi2005@users.noreply.github.com>
2026-06-23 22:35:07 +0530

    Time: 516 ms (37.44%), Space: 0B (100%) - LeetHub

---
+++ 0584-find-customer-referee/0584-find-customer-referee.sql
@@ -0,0 +1,3 @@
+select name from customer 
+where referee_id !=2 or  referee_id is null ; 
+-- in this question is asking that the customer referee_id =2 not equal so if we need to check is the value is null we will use (is null ) or referee_id ! = 2 !(to check if the value is 2 or not  )
